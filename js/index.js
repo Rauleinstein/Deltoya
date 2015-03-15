0972c36fbd9597c7f2b9abc1c902805e3ed4b2be
rauleinstein <rauleinstein@hotmail.com>
2015-03-15 01:08:33 +0100

    Arreglo del input 2

--- js/index.js
+++ js/index.js
@@ -69,7 +69,50 @@
         unlist();
     }
 
+    function solucionInput () {
+               var ver = window.navigator.appVersion;
+            ver = ver.toLowerCase();
+
+        if ( ver.indexOf("android 4.1") >= 0 ){            
+
+            var idMaxLengthMap = {};
+
+            //loop through all input-text and textarea element
+            $.each($(':text, textarea, :password'), function () {
+                var id = $(this).attr('id'),
+                    maxlength = $(this).attr('maxlength');
+
+                //element should have id and maxlength attribute
+                if ((typeof id !== 'undefined') && (typeof maxlength !== 'undefined')) {
+                    idMaxLengthMap[id] = maxlength;
+
+                    //remove maxlength attribute from element
+                    $(this).removeAttr('maxlength');
+
+                    //replace maxlength attribute with onkeypress event
+                    $(this).attr('onkeypress','if(this.value.length >= maxlength ) return false;');
+                }
+            });
+
+            //bind onchange & onkeyup events
+            //This events prevents user from pasting text with length more then maxlength
+            $(':text, textarea, :password').bind('change keyup', function () {
+                var id = $(this).attr('id'),
+                    maxlength = '';
+                if (typeof id !== 'undefined' && idMaxLengthMap.hasOwnProperty(id)) {
+                    maxlength = idMaxLengthMap[id];
+                    if ($(this).val().length > maxlength) {
+
+                        //remove extra text which is more then maxlength
+                        $(this).val($(this).val().slice(0, maxlength));
+                    }
+                }
+            });
+        }
+    }
+
     $( document ).ready(function() {
+        solucionInput();
     	nombre = window.localStorage.getItem("nombre");
     	telefono = window.localStorage.getItem("telefono"); 	
     	$("#nombre").val(nombre);
